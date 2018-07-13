e4daa26969ed1ced36295fde06b51738b132e86c
Cindy Vo <yendai.vo@hotmail.com>
2018-07-13 15:57:03 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,7 +1,10 @@
 def my_each(array)
-  i = 0
-  while i < array.length
-    yield array[i]
-    i = i + 1
+  if block_given?
+    i = 0
+    while i < array.length
+      yield array[i]
+      i = i + 1
+    end
+  else array
   end
 end
